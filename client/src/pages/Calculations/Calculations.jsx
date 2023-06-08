417be20c5e0ab6638165dde75e0c2c0e54a84397
unknown <justinasve@gmail.com>
2023-06-08 16:46:44 +0300

    fixed accounts functionality

--- client/src/pages/Calculations/Calculations.jsx
+++ client/src/pages/Calculations/Calculations.jsx
@@ -1,17 +1,22 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../contexts/UserContextWrapper";
 
 export const Calculations = () => {
     const [calculations, setCalculations] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    // const [projectHours, setProjectHours] = useState('');
+    // const [deadline, setDeadline] = useState('');
+    // const [result, setResult] = useState('');
+    const { user } = useContext(UserContext);
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}/calculations?userId=3`)
+        fetch(`${process.env.REACT_APP_API_URL}/calculations?userId=${user.id}`)
         .then(res => res.json())
         .then(data => {
             setCalculations(data);
             setIsLoading(false);
         });
-    }, []);
+    }, [user.id]);
 
     if (isLoading) {
         return <div>Loading...</div>;
