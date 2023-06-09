d614d5f3d06de84663926ce4ded24aa687e8db34
unknown <justinasve@gmail.com>
2023-06-09 06:28:25 +0300

    Added calculations page but no results

--- client/src/pages/Calculations/Calculations.jsx
+++ client/src/pages/Calculations/Calculations.jsx
@@ -4,9 +4,11 @@ import { UserContext } from "../../contexts/UserContextWrapper";
 export const Calculations = () => {
     const [calculations, setCalculations] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    // const [projectHours, setProjectHours] = useState('');
-    // const [deadline, setDeadline] = useState('');
-    // const [result, setResult] = useState('');
+    const [projectHours, setProjectHours] = useState(0);
+    const [deadline, setDeadline] = useState('');
+    const [commitments, setCommitments] = useState([]);
+    const [maxDailyHours, setMaxDailyHours] = useState(8);
+    const [result, setResult] = useState(null);
     const { user } = useContext(UserContext);
 
     useEffect(() => {
@@ -22,14 +24,215 @@ export const Calculations = () => {
         return <div>Loading...</div>;
     };
 
-    return (
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      fetch(`${process.env.REACT_APP_API_URL}/calculations`, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              projectHours,
+              deadline,
+              commitments,
+              maxDailyHours,
+              userId: user.id
+          })
+      })
+      .then((res) => res.json())
+      .then((data) => {
+          setCalculations(data);
+          setProjectHours('');
+          setDeadline('');
+          setCommitments('');
+          setMaxDailyHours('');
+      });
+  }
+  const handleProjectHoursChange = (event) => {
+    const hours = Math.max(0, parseInt(event.target.value));
+    setProjectHours(hours);
+  };
+
+  const handleDeadlineChange = (event) => {
+    setDeadline(event.target.value);
+  };
+
+  const handleCommitmentDateChange = (event, index) => {
+    const updatedCommitments = [...commitments];
+    updatedCommitments[index].date = event.target.value;
+    setCommitments(updatedCommitments);
+  };
+
+  const handleCommitmentHoursChange = (event, index) => {
+    const hours = Math.max(0, Math.min(parseInt(event.target.value), maxDailyHours));
+    const updatedCommitments = [...commitments];
+    updatedCommitments[index].hours = hours;
+    setCommitments(updatedCommitments);
+  };
+
+  const addCommitment = () => {
+    setCommitments([...commitments, { date: '', hours: 0 }]);
+  };
+
+  const deleteCommitment = (index) => {
+    const updatedCommitments = [...commitments];
+    updatedCommitments.splice(index, 1);
+    setCommitments(updatedCommitments);
+  };
+
+  const handleMaxDailyHoursChange = (event) => {
+    const hours = Math.max(0, parseInt(event.target.value));
+    setMaxDailyHours(hours);
+  };
+
+  const calculateRemainingTime = () => {
+    const today = new Date();
+    const deadlineDate = new Date(deadline);
+    const timeDiff = deadlineDate.getTime() - today.getTime();
+    const remainingDays = Math.floor(timeDiff / (1000 * 3600 * 24));
+
+    if (remainingDays >= 0) {
+      const totalCommitmentHours = commitments.reduce(
+        (total, commitment) => total + parseInt(commitment.hours),
+        0
+      );
+
+      const remainingWorkHours = Math.max(0, projectHours - totalCommitmentHours);
+      const availableDays = Math.ceil(remainingWorkHours / maxDailyHours);
+
+      if (availableDays <= remainingDays) {
+        const workSchedule = [];
+
+        let remainingWorkHoursRemainingDays = remainingWorkHours;
+
+        for (let i = 1; i < remainingDays; i++) { // Start from the second day
+          const date = new Date(today.getTime() + i * 24 * 3600 * 1000);
+          const commitmentOnDate = commitments.find(
+            (commitment) => commitment.date === date.toISOString().slice(0, 10)
+          );
+
+          let workHours = maxDailyHours;
+          if (commitmentOnDate) {
+            const commitmentHours = commitmentOnDate.hours;
+            workHours = Math.max(0, workHours - commitmentHours);
+          }
+
+          if (remainingWorkHoursRemainingDays > 0) {
+            workHours = Math.min(workHours, remainingWorkHoursRemainingDays);
+            remainingWorkHoursRemainingDays -= workHours;
+          } else {
+            workHours = 0;
+          }
+
+          workSchedule.push({
+            date: date.toDateString(),
+            hours: workHours,
+          });
+        }
+
+        return {
+          canFinishOnTime: true,
+          workSchedule: workSchedule,
+        };
+      }
+    }
+
+    return {
+      canFinishOnTime: false,
+      workSchedule: [],
+    };
+  };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const result = calculateRemainingTime();
+  //   setResult(result);
+  // };
+
+  return (
+    <div>
+      <h1>Time Management App</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Project Hours:
+          <input
+            type="number"
+            value={projectHours}
+            onChange={handleProjectHoursChange}
+          />
+        </label>
+        <br />
+        <label>
+          Deadline:
+          <input
+            type="date"
+            value={deadline}
+            onChange={handleDeadlineChange}
+          />
+        </label>
+        <br />
+        <label>
+          Commitments:
+          {commitments.map((commitment, index) => (
+            <div key={index}>
+              <label>
+                Date:
+                <input
+                  type="date"
+                  value={commitment.date}
+                  onChange={(event) => handleCommitmentDateChange(event, index)}
+                />
+              </label>
+              <label>
+                Hours:
+                <input
+                  type="number"
+                  value={commitment.hours}
+                  onChange={(event) => handleCommitmentHoursChange(event, index)}
+                />
+              </label>
+              <button type="button" onClick={() => deleteCommitment(index)}>
+                Delete
+              </button>
+            </div>
+          ))}
+        </label>
+        <br />
+        <button type="button" onClick={addCommitment}>
+          Add Commitment
+        </button>
+        <br />
+        <label>
+          Max Daily Hours:
+          <input
+            type="number"
+            value={maxDailyHours}
+            onChange={handleMaxDailyHoursChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Calculate</button>
+      </form>
+      {result && (
         <div>
-            {calculations.map((calculation) =>
-                <div key={calculation.id}>
-                    <span>{calculation.projectHours}</span>&ensp;
-                    <span>{calculation.hours}</span>
-                </div>
-            )}
+          <h2>Result:</h2>
+          {result.canFinishOnTime ? (
+            <div>
+              <p>You can finish the project on time.</p>
+              <p>Work Schedule:</p>
+              <ul>
+                {result.workSchedule.map((schedule, index) => (
+                  <li key={index}>
+                    {schedule.date}: {schedule.hours} hours
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p>You cannot finish the project on time.</p>
+          )}
         </div>
-    );
+      )}
+    </div>
+  );
 };
